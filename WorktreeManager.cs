ffc5df57af0be2fdea409f7d479780f8a029ce60
Logan Bussell <loganbussell@microsoft.com>
2026-04-15 09:26:16 -0700

    Add result monad

--- WorktreeManager.cs
+++ WorktreeManager.cs
@@ -7,3 +7,42 @@
 #:package CliWrap@3.*
 
 Console.WriteLine("Hello world!");
+
+public abstract record Result<T>
+{
+    public sealed record Ok(T Value) : Result<T>;
+    public sealed record Error(string Message) : Result<T>;
+
+    public static Result<T> Success(T value) => new Ok(value);
+    public static Result<T> Failure(string message) => new Error(message);
+
+    public Result<U> Map<U>(Func<T, U> f) => this switch
+    {
+        Ok(var v) => Result<U>.Success(f(v)),
+        Error(var msg) => Result<U>.Failure(msg),
+        _ => throw new InvalidOperationException()
+    };
+
+    public Result<U> Bind<U>(Func<T, Result<U>> f) => this switch
+    {
+        Ok(var v) => f(v),
+        Error(var msg) => Result<U>.Failure(msg),
+        _ => throw new InvalidOperationException()
+    };
+
+    public T Unwrap() => this switch
+    {
+        Ok(var v) => v,
+        Error(var msg) => throw new InvalidOperationException($"Unwrap called on Error: {msg}"),
+        _ => throw new InvalidOperationException()
+    };
+
+    public T UnwrapOr(T fallback) => this switch
+    {
+        Ok(var v) => v,
+        _ => fallback
+    };
+
+    public bool IsOk => this is Ok;
+    public bool IsError => this is Error;
+}
